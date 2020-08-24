9105611b8cb272ebd3d547f7125e54d5a7267b07
Aditya Mukherjee <adityamukherjee98@gmail.com>
2020-08-24 13:01:08 +0530

    Modified settings file to accept username and password from config.json

--- django_project/settings.py
+++ django_project/settings.py
@@ -142,8 +142,5 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-# EMAIL_HOST_USER: "adityamukherjee98@gmail.com"
-# EMAIL_HOST_PASSWORD: "RedhatLinux1"
-
 EMAIL_HOST_USER = params["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = params["EMAIL_HOST_PASSWORD"]
